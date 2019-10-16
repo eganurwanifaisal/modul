32ad6d59dee8a11fd97d93305cecc5e9d45bc0f4
eganurwanifaisal <49120750+eganurwanifaisal@users.noreply.github.com>
2019-10-16 17:47:18 +0800

    Add files via upload

---
+++ bangunruang.py
@@ -0,0 +1,13 @@
+import math 
+
+def menghitungluaspermukaantabung (r, t) :
+	return  2 * math.pi * r * r * t
+
+def menghitungluaspermukaanbalok (p,l,t) :
+	return  2 *(p+l+t)
+
+def menghitungluaspermukaankubus (s) :
+	return  6 * s * s
+
+def menghitungluaspermukaanbola (r) :
+	return  4 * math.pi * r * r
